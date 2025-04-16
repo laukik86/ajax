3fa955c8afa2680d288f51cb746b2e7a485ae9c4
laukik86 <laukikpadgaonkar0@gmail.com>
2025-04-16 21:10:26 +0530

    aray

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(__dirname));
-app.use(express.json());
+app.use(bodyParser.json());
 
-let users = [];
-const colleges = ["VESIT", "VJTI", "IIT Bombay", "SPIT", "DJ Sanghvi", "KJ Somaiya", "Thadomal Shahani"];
+let existingUsers = ["p diddy", "atharva39", "little kid lover", "aman", "laukik", "robert de niro", "clint_eastwood", "devilmaycry", "horse_ridder", "malehunter"];
 
-app.get('/usernames', (req, res) => {
-  const usernames = users.map(user => user.username);
-  res.json(usernames);
-});
-
-app.get('/suggest-college', (req, res) => {
-  const query = req.query.query.toLowerCase();
-  const filtered = colleges.filter(c => c.toLowerCase().includes(query));
-  res.json(filtered);
+app.post('/check-username', (req, res) => {
+  const { username } = req.body;
+  if (existingUsers.includes(username.toLowerCase())) {
+    res.send("Username already taken");
+  } else {
+    res.send("");
+  }
 });
 
 app.post('/register', (req, res) => {
-  const { name, college, username, password } = req.body;
-  users.push({ name, college, username, password });
-  res.sendStatus(200);
+  const { username } = req.body;
+  if (existingUsers.includes(username.toLowerCase())) {
+    return res.send("Username already exists. Try another.");
+  }
+  existingUsers.push(username.toLowerCase()); // Save new user
+  res.send("Successfully Registered");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
